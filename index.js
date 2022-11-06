15312fc0a7503d039cd01e4246015e069bc61f0e
blueStragglr <Oortcloud1599@gmail.com>
2022-11-06 20:44:26 +0900

    chore: rebuild package

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const Chevron_1 = __importDefault(require("./ChevronRight/Chevron"));
+const Chevron_1 = __importDefault(require("./Chevron/Chevron"));
 exports.default = Chevron_1.default;
